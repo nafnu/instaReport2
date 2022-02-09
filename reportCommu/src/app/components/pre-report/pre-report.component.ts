135c0f9903be724971159f7d67e9bd8177546d69
Nathalie Flores <65398774+nafnu@users.noreply.github.com>
2022-02-09 18:44:45 +0000

    ionic-angular

---
+++ reportCommu/src/app/components/pre-report/pre-report.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pre-report',
+  templateUrl: './pre-report.component.html',
+  styleUrls: ['./pre-report.component.scss'],
+})
+export class PreReportComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
---
+++ reportCommu/src/app/components/type/type.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-type',
+  templateUrl: './type.component.html',
+  styleUrls: ['./type.component.scss'],
+})
+export class TypeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
