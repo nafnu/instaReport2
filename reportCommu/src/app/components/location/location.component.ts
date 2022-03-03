651d56bd55e38aa475c67b73d9633ada592c3bdf
Nathalie Flores <65398774+nafnu@users.noreply.github.com>
2022-03-03 20:06:34 +0000

    commit

--- reportCommu/src/app/components/location/location.component.ts
+++ reportCommu/src/app/components/location/location.component.ts
@@ -1,4 +1,7 @@
 import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { Camera, CameraResultType, CameraSource } from '@capacitor/camera';
+import { base64 } from '@firebase/util';
+import { $ } from 'protractor';
 
 @Component({
   selector: 'app-location',
@@ -8,6 +11,9 @@ import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 export class LocationComponent implements OnInit {
 
   @ViewChild('map') mapView:ElementRef;
+
+  title = 'angularCapacitor'; 
+  image = '';
 
   constructor() { }
 
@@ -21,6 +27,19 @@ export class LocationComponent implements OnInit {
     const boundingRect = this.mapView.nativeElement.getBoundingClientRect() as DOMRect;
     console.log("yeah", boundingRect)
 
+  }
+
+  async captureImage(){
+    const image = await Camera.getPhoto({
+      quality: 90,
+      allowEditing: true,
+      source: CameraSource.Prompt,
+      resultType: CameraResultType.Base64
+    });
+  }
+
+  if(image){
+    this.image = `data:image/jpeg;base64,${image.base64String}`!;
   }
   
 }
