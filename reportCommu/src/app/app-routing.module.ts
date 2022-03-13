91273f17d22c212e4c1e407d8700373360245e16
Nathalie Flores <65398774+nafnu@users.noreply.github.com>
2022-03-13 16:14:29 +0000

    commit

--- reportCommu/src/app/app-routing.module.ts
+++ reportCommu/src/app/app-routing.module.ts
@@ -12,7 +12,7 @@ import { TypeComponent } from './components/type/type.component';
 
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
-import{ redirectUnauthorizedTo, redirectLoggedInTo, canActivate } from '@angular/fire/auth-guard';
+import{ redirectUnauthorizedTo, redirectLoggedInTo, canActivate, AuthGuard } from '@angular/fire/auth-guard';
 import { SplashComponent } from './components/splash/splash.component';
 
 
@@ -22,13 +22,27 @@ const redirectLoggedInToHome = () => redirectLoggedInTo(['home']);
 
 const routes: Routes = [
   {path:'', component:SplashComponent},
-  {path:'login', component:LoginComponent},
-  {path:'register', component:RegisterComponent},
-  {path:'home', component:HomeComponent},
+  
+  {path:'login', component:LoginComponent,
+  // canActivate:[AuthGuard], 
+  // data:{component: HomeComponent}
+  ...canActivate(redirectLoggedInToHome)
+   },
+
+  {path:'register', component:RegisterComponent,
+  ...canActivate(redirectLoggedInToHome)},
+  
+  {path:'home', component:HomeComponent,
+  ...canActivate(redirectUnauthorizedToLogin)},
+
   {path:'location', component: LocationComponent},
+
   {path:'type', component: TypeComponent},
+
   {path:'details', component: DetailsComponent},
+
   {path:'summary', component:SummaryComponent},
+  
   {path:'restart', component:RestartComponent}
 
 ];
