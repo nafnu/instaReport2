51fe43aa040ffd20f978ed16b3d311d3380da486
Nathalie Flores <65398774+nafnu@users.noreply.github.com>
2022-03-16 12:35:00 +0000

    commit

--- reportCommu/src/app/services/db.service.ts
+++ reportCommu/src/app/services/db.service.ts
@@ -1,7 +1,18 @@
 import { Injectable } from '@angular/core';
-import { collectionData, Firestore, doc, getFirestore ,addDoc, collection, deleteDoc, updateDoc } from '@angular/fire/firestore';
-import { docData } from 'rxfire/firestore';
+import { user } from '@angular/fire/auth';
+import { collectionData, Firestore, doc, getFirestore ,addDoc, collection, deleteDoc, updateDoc, docData } from '@angular/fire/firestore';
+
 import { Observable, BehaviorSubject} from 'rxjs';
+
+export interface Profile {
+  uid?: string,
+  lname: string,
+  fname: string,
+  phone: number, 
+  city: string,
+ street: string,
+ eircode:string
+}
 
 
 @Injectable({
@@ -11,8 +22,31 @@ export class DbService {
 
  
 
-  constructor() { }
+  constructor(private firestore: Firestore) { }
 
+
+  //Get all the information of the users
+  getProfile(): Observable<Profile[]> {
+    const profileRef = collection(this.firestore, 'users');
+    return collectionData(profileRef, { idField: 'id' }) as Observable<Profile[]>;
+  }
+
+  //Get all the information of one users by ID
+  getProfileById(id): Observable<Profile[]> {
+    const profileDocRef = doc(this.firestore, `users/${id}`);
+    return docData(profileDocRef, { idField: 'id' }) as Observable<Profile[]>;
+  }
+
+  addProfile(profile: Profile){
+    const profileRef = collection(this.firestore, 'users');
+    return addDoc(profileRef, profile);
+  }
+
+  deleteProfile(profile: Profile){
+    const profileDocRef = doc(this.firestore, `users/${profile.uid}`);
+    return deleteDoc(profileDocRef);
+  }
+  
 
 
 }
