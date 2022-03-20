9ce3805a1125c5257b08a85a5d1b70e21c84b526
Nathalie Flores <65398774+nafnu@users.noreply.github.com>
2022-03-20 22:40:23 +0000

    commit

--- reportCommu/src/app/components/googlemaps/googlemaps.component.ts
+++ reportCommu/src/app/components/googlemaps/googlemaps.component.ts
@@ -1,9 +1,10 @@
 import { DOCUMENT } from '@angular/common';
 import { Component, ElementRef, Inject, Input, OnInit, Renderer2, ViewChild } from '@angular/core';
-import { Geolocation } from '@ionic-native/geolocation/ngx';
+//import { Geolocation } from '@ionic-native/geolocation/ngx';
 import { ModalController } from '@ionic/angular';
 import { CapacitorGoogleMaps } from '@capacitor-community/capacitor-googlemaps-native';
 import { GooglemapsService } from 'src/app/components/googlemaps/googlemaps.service';
+import { Geolocation } from '@capacitor/geolocation';
 
 @Component({
   selector: 'app-googlemaps',
@@ -30,9 +31,38 @@ export class GooglemapsComponent implements OnInit {
       height: Math.round(boundingRect.height),
       x: Math.round(boundingRect.x),
       y: Math.round(boundingRect.y),
-      latitude: 53.350140,
-      longitude: -6.266155,
+      // latitude: 53.350140,
+      // longitude: -6.266155,
       zoom: 5
-    })
+    });
+
+    CapacitorGoogleMaps.addListener('onMapReady', async () => {
+      CapacitorGoogleMaps.setMapType({
+        type: 'hybrid'
+      });
+
+      //this.showCurrentPosition();
+    });
   }
+
+  // showCurrentPosition(){
+  //  Geolocation.requestPermissions().then(async permission => {
+  //    const coordinates = await Geolocation.getCurrentPosition();
+
+  //    CapacitorGoogleMaps.addMarker({
+  //      latitude: coordinates.coords.latitude,
+  //      longitude: coordinates.coords.longitude,
+  //      title: 'Location',
+  //      snippet: 'The location of the incident'
+  //    });
+
+  //    CapacitorGoogleMaps.setCamera({
+  //     latitude: coordinates.coords.latitude,
+  //     longitude: coordinates.coords.longitude,
+  //     zoom: 12, 
+  //     bearing: 0
+  //    });
+
+  //  });
+  // }
 }
