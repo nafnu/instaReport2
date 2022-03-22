0ae33a06dbfdc050e500f0938fb7fac05b3753c3
Nathalie Flores <65398774+nafnu@users.noreply.github.com>
2022-03-22 10:22:30 +0000

    commit

--- reportCommu/src/app/components/googlemaps/googlemaps.component.ts
+++ reportCommu/src/app/components/googlemaps/googlemaps.component.ts
@@ -2,7 +2,7 @@ import { DOCUMENT } from '@angular/common';
 import { Component, ElementRef, Inject, Input, OnInit, Renderer2, ViewChild } from '@angular/core';
 //import { Geolocation } from '@ionic-native/geolocation/ngx';
 import { ModalController } from '@ionic/angular';
-import { CapacitorGoogleMaps } from '@capacitor-community/capacitor-googlemaps-native';
+//import { CapacitorGoogleMaps } from '@capacitor-community/capacitor-googlemaps-native';
 import { GooglemapsService } from 'src/app/components/googlemaps/googlemaps.service';
 import { Geolocation } from '@capacitor/geolocation';
 
@@ -19,31 +19,31 @@ export class GooglemapsComponent implements OnInit {
  
   ngOnInit() { }
 
-  ionViewDidEnter(){
-    this.createMap();
-  }
+  // ionViewDidEnter(){
+  //   this.createMap();
+  // }
 
-  createMap(){
-    const boundingRect = this.mapView.nativeElement.getBoundingClientRect() as DOMRect;
-    console.log("createMap", boundingRect)
-    CapacitorGoogleMaps.create({
-      width: Math.round(boundingRect.width),
-      height: Math.round(boundingRect.height),
-      x: Math.round(boundingRect.x),
-      y: Math.round(boundingRect.y),
-      // latitude: 53.350140,
-      // longitude: -6.266155,
-      zoom: 5
-    });
+  // createMap(){
+  //   const boundingRect = this.mapView.nativeElement.getBoundingClientRect() as DOMRect;
+  //   console.log("createMap", boundingRect)
+  //   CapacitorGoogleMaps.create({
+  //     width: Math.round(boundingRect.width),
+  //     height: Math.round(boundingRect.height),
+  //     x: Math.round(boundingRect.x),
+  //     y: Math.round(boundingRect.y),
+  //     // latitude: 53.350140,
+  //     // longitude: -6.266155,
+  //     zoom: 5
+  //   });
 
-    CapacitorGoogleMaps.addListener('onMapReady', async () => {
-      CapacitorGoogleMaps.setMapType({
-        type: 'hybrid'
-      });
+  //   CapacitorGoogleMaps.addListener('onMapReady', async () => {
+  //     CapacitorGoogleMaps.setMapType({
+  //       type: 'hybrid'
+  //     });
 
-      //this.showCurrentPosition();
-    });
-  }
+  //     //this.showCurrentPosition();
+  //   });
+  // }
 
   // showCurrentPosition(){
   //  Geolocation.requestPermissions().then(async permission => {
