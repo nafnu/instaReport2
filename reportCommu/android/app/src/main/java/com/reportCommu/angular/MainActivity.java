9c5f16176128e0b61676ce871d226b0df3a32d55
Nathalie Flores <65398774+nafnu@users.noreply.github.com>
2022-03-22 12:04:37 +0000

    commit

--- reportCommu/android/app/src/main/java/com/reportCommu/angular/MainActivity.java
+++ reportCommu/android/app/src/main/java/com/reportCommu/angular/MainActivity.java
@@ -3,11 +3,7 @@ package com.reportCommu.angular;
 import android.os.Bundle;
 
 import com.getcapacitor.BridgeActivity;
-import com.hemangkumar.capacitorgooglemaps.CapacitorGoogleMaps;
 
 public class MainActivity extends BridgeActivity {
-    public void OnCreate(Bundle savedInstanceState){
-        super.onCreate(savedInstanceState);
-        registerPlugin(CapacitorGoogleMaps.class);
-    }
+
 }
