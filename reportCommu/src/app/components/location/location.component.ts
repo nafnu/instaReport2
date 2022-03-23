9b6ef19896f44c18a1bce759a78a504c6aa929b2
Nathalie Flores <65398774+nafnu@users.noreply.github.com>
2022-03-23 11:09:24 +0000

    commit

--- reportCommu/src/app/components/location/location.component.ts
+++ reportCommu/src/app/components/location/location.component.ts
@@ -1,14 +1,21 @@
 import { DOCUMENT } from '@angular/common';
 import { Component, ElementRef, Inject, Input, OnInit, Renderer2, ViewChild } from '@angular/core';
-import { Camera, CameraResultType, CameraSource } from '@capacitor/camera';
-import { Geolocation } from '@ionic-native/geolocation/ngx';
-import { ModalController } from '@ionic/angular';
-//import { GooglemapsService } from 'src/app/components/googlemaps/googlemaps.service';
+import { ModalController, LoadingController, AlertController } from '@ionic/angular';
 
+//Imports to save the information in the database
+import { AuthService } from 'src/app/services/auth.service';
+import { DbService } from 'src/app/services/db.service';
 import { Report } from 'src/app/models/models';
 
-//import { CapacitorGoogleMaps } from '@capacitor-community/capacitor-googlemaps-native';
+//Imports for the camera
+import { Camera, CameraResultType, CameraSource } from '@capacitor/camera';
+
+//Imports for the map
 import { GooglemapsComponent } from 'src/app/components/googlemaps/googlemaps.component';
+import { loadingController } from '@ionic/core';
+//import { GooglemapsService } from 'src/app/components/googlemaps/googlemaps.service';
+//import { Geolocation } from '@ionic-native/geolocation/ngx';
+//import { CapacitorGoogleMaps } from '@capacitor-community/capacitor-googlemaps-native';
 
 
 @Component({
@@ -28,13 +35,21 @@ export class LocationComponent implements OnInit {
     authority: '',
   }
 
-  files = [];
-
+  profile = null;
+  
   @ViewChild('map') mapView: ElementRef;
 
   constructor(
     public modalCtrl: ModalController,
-  ) { }
+    public loadingController: LoadingController,
+    public alertController: AlertController,
+    private authService: AuthService,
+    private db: DbService
+  ) { 
+    this.authService.getUserProfile().subscribe(data => {
+      this.profile = data;
+    })
+  }
 
   ngOnInit() {  }
 
@@ -49,9 +64,29 @@ export class LocationComponent implements OnInit {
    const image = await Camera.getPhoto({
      quality: 90,
      allowEditing: true,
-     source: CameraSource.Prompt,
-     resultType: CameraResultType.Base64
+     source: CameraSource.Photos, //Camera, photos or prompt!
+     resultType: CameraResultType.Base64,
    });
+
+   console.log(image);
+
+   if (image){
+    const loading = await this.loadingController.create();
+    await loading.present();
+
+    const result = await this.db.uploadImage(image);
+    loading.dismiss();
+
+    if(!result){
+      const alert = await this.alertController.create({
+        header: 'Upload failed', 
+        message: 'There was a problem uploading your image.', 
+        buttons: ['OK'],
+      });
+      await alert.present();
+    }
+   }
+
  }
 
  if(image) {
