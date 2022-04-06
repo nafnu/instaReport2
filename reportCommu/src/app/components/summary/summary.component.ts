7513080048c5d983356f362f157515b7e4ee833e
Nathalie Flores <65398774+nafnu@users.noreply.github.com>
2022-04-06 15:13:54 +0100

    commit

--- reportCommu/src/app/components/summary/summary.component.ts
+++ reportCommu/src/app/components/summary/summary.component.ts
@@ -2,10 +2,11 @@ import { Component, Directive, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { AlertController } from '@ionic/angular';
 import { DbService, User } from 'src/app/services/db.service';
+import { Report } from 'src/app/models/models';
 
 import { Auth, getAuth, onAuthStateChanged } from '@angular/fire/auth';
 
-import { EmailComposer } from '@ionic-native/email-composer/ngx'
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
 
 
 @Component({
@@ -20,13 +21,16 @@ export class SummaryComponent implements OnInit {
   lat: string;
   long: string;
   incident: string;
-  notes: string;
+  note: string;
   council: string;
+  imagen: string;
 
   //variable to connect get user info (email, name)
   user: User = null;
-users = [];
-  id: string;
+  users = [];
+
+
+  summary = [];
 
   //variables to create in emailcomposer
   subject: string;
@@ -34,15 +38,18 @@ users = [];
   to: string;
 
   //interfaces to save information on Firebase
-  // summary: Report = {
-  //   uid: null,
-  //   lat: null,
-  //   lng: null,
-  //   imagen: null,
-  //   incident: null, 
-  //   description: null,
-  //   authority: null,
-  // }
+   report: Report = {
+    id: null,
+     uid: null,
+     lat: null,
+     lng: null,
+     imagen: null,
+     description: null,
+     authority: null,
+     incident: null,
+  }
+
+   reportForm: FormGroup;
 
   constructor(
     public dataService: DbService,
@@ -50,8 +57,7 @@ users = [];
     public alertContrl: AlertController,
     public activatedRoute: ActivatedRoute,
     public auth: Auth,
-    public composer: EmailComposer,
-    
+    public formBuilder: FormBuilder
   ) {
     //reach the users collection in firebase
     this.dataService.getUser().subscribe(res => {
@@ -59,46 +65,51 @@ users = [];
       this.users = res;
     });
 
-    
-   
-    // this.dataService.getUserById(this.value).subscribe(res => {
-    //   this.currentU = res;
-    // })
-   
- 
+    this.dataService.getReports().subscribe(res => {
+      console.log(res);
+      this.summary = res;
+    })
+
+
+
+
+
   }
 
- 
- //check if the mobile has an app to send email //**The function sent email works but in the phone no in the emulator */
-//  this.composer.isAvailable().then((available: boolean) =>{
-//   if(available) {
-//     this.sendEmail();
-//   }
-//  });
+
+
 
   ngOnInit() {
     this.passedIdD = this.activatedRoute.snapshot.paramMap.get('uid');
     this.lat = this.activatedRoute.snapshot.paramMap.get('lat');
     this.long = this.activatedRoute.snapshot.paramMap.get('long');
     this.incident = this.activatedRoute.snapshot.paramMap.get('data');
-    this.notes = this.activatedRoute.snapshot.paramMap.get('notes');
+    this.note = this.activatedRoute.snapshot.paramMap.get('pass');
     console.log(this.passedIdD);
     console.log(this.lat);
     console.log(this.long);
     console.log(this.incident);
+    console.log(this.note);  // Error to carry the note
 
     this.getConuncil();
     console.log(this.council);
     console.log(this.to);
 
-   
+
     this.getUid();
 
     this.getUserInfo();
 
+    this.reportForm = this.formBuilder.group({ 
+      council: ['', [Validators.required, Validators.maxLength(100)]],
+      incident: ['', [Validators.required, Validators.maxLength(100)]],
+      lat: ['', [Validators.required, Validators.pattern('[- +()0-9]+')]],
+      long: ['', [Validators.required, Validators.pattern('[- +()0-9]+')]],
+    });
+
   }
 
-  
+
   //Estimate of authority/email according to long 
   getConuncil() {
     const str = this.long;
@@ -111,15 +122,15 @@ users = [];
     const dunlaL = 53.291676;
     const dcityL = 55.184590;
 
-   if ( float <= dunla ){
-    this.council = "Dún Laoghaire County Council";
-    this.to = "info@dlrcoco.ie"
-   } else if (float <= dcity){
-     this.council = "Dublin City Council";
-     this.to = "customerservices@dublincity.ie";
-   }
+    if (float <= dunla) {
+      this.council = "Dún Laoghaire County Council";
+      this.to = "info@dlrcoco.ie"
+    } else if (float <= dcity) {
+      this.council = "Dublin City Council";
+      this.to = "customerservices@dublincity.ie";
+    }
 
-  
+
   }
 
   getUid(): void {
@@ -127,62 +138,44 @@ users = [];
     onAuthStateChanged(auth, (user) => {
       if (user) {
         const uid = user.uid;
-        this.id = uid;
-        console.log(this.id); 
+        this.passedIdD = uid;
+        console.log(this.passedIdD);
       }
       return ('id');
     });
 
   }
-  
-  getUserInfo(){
+
+  getUserInfo() {
 
     // this.dataService.getUser().subscribe(res => {
     //     console.log(res);
     //     this.users = res;
     //   });
-    
+
     this.activatedRoute.params.subscribe(params => {
-      this.id = params ['id'];
-      this.dataService.getUserById(this.id).subscribe(res => {
+      this.passedIdD = params['id'];
+      this.dataService.getUserById(this.passedIdD).subscribe(res => {
         this.user = res;
         console.log(this.user);
-      }) 
+      })
     })
+
+  }
+
+  async saveRegister(){
+    // this.report = await this.reportForm.value;
+    console.log(this.report); 
+
+    this.report.authority = this.council;
+    this.report.description= "this.note";
+    this.report.uid = this.passedIdD;
+    this.report.lat = parseInt(this.lat);
+    this.report.lng = parseInt(this.long);
+    this.report.incident = this.incident;
+    this.report.imagen = "this.imagen";
+       
+    await this.dataService.createReport(this.report);
     
-}  
-  /*** Part to send email */
-  
-  // sendEmail(){
-  //   //The notes of the user are part in the body
-  //   this.body = this.notes;
-
-  //      //check if the mobile has an app to send email //**The function sent email works but in the phone no in the emulator */
-  //      this.composer.isAvailable().then(function() {
-  //       console.log("email available");
-  //         }, function() {
-  //       console.log("email not availabl");
-  //     });
-
-  //    let email = {
-  //     to: '21520@student.dorset-college.ie  ',  //this.to //**but not implement coz it is a student project */
-  //     // cc: 'nafnu@hotmail.com', 
-  //     // bcc: ['john@doe.com', 'jane@doe.com'],
-  //     // attachments: [
-  //     //   'file://img/logo.png',
-  //     //   'res://icon.png',
-  //     //   'base64:icon.png//iVBORw0KGgoAAAANSUhEUg...',
-  //     //   'file://README.pdf'
-  //     // ],
-  //     subject: 'Test Email Ionic',  //this.incident
-  //     body: 'How are you? I am sending this message in order to resolve a problem.', //this.body
-  //     isHtml: true, 
-  //     app:"Gmail"
-  //   };
-  //   this.composer.open(email);
-
-    
-  // }
-
-
+  }
 }
